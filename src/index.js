db9ddff802b4c76c4e7284d5f0e96e5b251c9b9d
Austin Herman <aherman@sociusmarketing.com>
2019-05-04 00:46:58 -0400

    build out login page structure and style

--- src/index.js
+++ src/index.js
@@ -2,9 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Login from './pages/Login';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import GlobalStyle from './styles/global';
+import typography from './styles/typography';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+typography.injectStyles();
+
+const routing = (
+  <>
+    <GlobalStyle></GlobalStyle>
+    <Router>
+      <div>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
+      </div>
+    </Router>
+  </>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
